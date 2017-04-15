52b59e6325385889374fca65a940a1d1d1952703
ndalton12 <niall.dalton12@gmail.com>
2017-04-14 20:53:11 -0400

    Added music files

--- app/src/main/java/com/example/niall/game2/Menu.java
+++ app/src/main/java/com/example/niall/game2/Menu.java
@@ -13,6 +13,7 @@ public class Menu extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
