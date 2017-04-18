e024ad6c4bdaacfa5464c093a4796037da3b5260
ElenaCappy <elena@cappy.us>
2017-04-18 12:10:12 -0400

    question updated

--- app/src/main/java/com/example/niall/game2/Question.java
+++ app/src/main/java/com/example/niall/game2/Question.java
@@ -15,7 +15,7 @@ public class Question {
     public ArrayList<String[]> returner;
 
     public void Question(){
-        File inputFile = new File("/Users/ElenaCappy/Documents/AFG/AppsForGoodGame/app/src/main/assets/textQuestions.pdf");
+        File inputFile = new File("/Users/ElenaCappy/Documents/AFG/AppsForGoodGame/app/src/main/assets/txtQuestionSet.txt");
         Scanner in = null;
         try {
             in = new Scanner(inputFile);
