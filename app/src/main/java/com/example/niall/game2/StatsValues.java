f24f648c9d5def803018caad5ea90f4da9c39b76
ElenaCappy <elena@cappy.us>
2017-05-04 09:25:38 -0400

    Comments! -Elena

--- app/src/main/java/com/example/niall/game2/StatsValues.java
+++ app/src/main/java/com/example/niall/game2/StatsValues.java
@@ -5,22 +5,28 @@ public class StatsValues {
     private int totalMoney;
     private int numChoices;
 
+    //Contructor sets starter values of stats
     public StatsValues() {
         moneySpent = 0;
         totalMoney = 100;
         numChoices = 0;
     }
 
+    //increments the number of choices made by one
     public void updateNumChoices() {
         numChoices = numChoices +1;
     }
 
+    //changes the totalMoney variable based on the parameter
     public void updateMoney(int x) {
         totalMoney = totalMoney + x;
         if(x<0) {
             moneySpent = moneySpent+x;
         }
     }
+
+    //The following three methods are getters for the variables initialized at the top of this code.
+
     public int getTotalMoney() {
         return totalMoney;
     }
@@ -32,4 +38,5 @@ public class StatsValues {
     public int getNumChoices() {
         return numChoices;
     }
-        }
+
+}
